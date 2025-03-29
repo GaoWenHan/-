f6fc908aae5a690d0ea167f05c010b6ba5f145ff
gaowenhan <1049912732@qq.com>
2025-03-29 11:57:57 +0800

    gwh在/周考一/one添加了路由配置

---
+++ 周考一/one/my-app/src/router/index.jsx
@@ -0,0 +1,44 @@
+import { createHashRouter,Navigate } from 'react-router-dom';
+import Layouts from '../layouts';
+import Home from '../page/home';
+import Cate from '../page/cate';
+import Cart from '../page/cart';
+import My from '../page/my';
+
+
+const routes = createHashRouter([
+    {
+        path:'/',
+        element:<Navigate to='/layouts' replace />
+    },
+    {
+        path:'/layouts',
+        element:<Layouts/>,
+        children:[
+            {
+                index:true,
+                element:<Home/>
+            },
+            {
+                path:'home',
+                element:<Home/>
+            },
+            {
+                path:'cate',
+                element:<Cate/>
+            },
+            {
+                path:'cart',
+                element:<Cart/>
+            },
+            {
+                path:'my',
+                element:<My/>
+            }
+        ]
+    }
+])
+
+export {
+    routes
+}
