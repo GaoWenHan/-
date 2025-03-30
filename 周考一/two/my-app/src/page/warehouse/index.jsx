8decfe46d87f357194586900c623cd387e97849e
gaowenhan <1049912732@qq.com>
2025-03-30 21:08:06 +0800

    gwh增加了/周考一/two/my-app表单设置

--- 周考一/two/my-app/src/page/warehouse/index.jsx
+++ 周考一/two/my-app/src/page/warehouse/index.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import styles from "./index.module.css";
-import { Button, Modal } from "antd";
+import { Button, Modal, Input } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
+import pinyin from "pinyin";
 
 export default function WareHouse() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [warseForm, setWarseForm] = useState({
+    name: "",
+    code: "",
+    warseId: "",
+    Iphone: "",
+  });
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -14,27 +21,67 @@ export default function WareHouse() {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+  const handleChangeValue = (event) => {
+    const { name, value } = event.target;
+    setWarseForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handleNameBlur = () => {
+    const { name, code } = warseForm;
+    if (!code && name) {
+      const firstLetters = pinyin(name, {
+        style: pinyin.STYLE_FIRST_LETTER,
+      }).map(item => item[0]).join("");
+      setWarseForm((prev) => ({
+        ...prev,
+        code: firstLetters.toUpperCase(),
+      }));
+    }
+  };
   return (
     <>
       <div className={styles.warseBox}>
         <div className={styles.HeaderTop}>
-          <Button 
+          <Button
             onClick={showModal}
             style={{
-                width:'100px',
-                height:'45px'
+              width: "100px",
+              height: "45px",
             }}
-            ><PlusOutlined />添加</Button>
+          >
+            <PlusOutlined />
+            添加
+          </Button>
         </div>
         <Modal
-          title="Basic Modal"
+          title="添加仓库"
           open={isModalOpen}
           onOk={handleOk}
           onCancel={handleCancel}
         >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
+          <p>
+            仓库名称:
+            <Input
+              onBlur={handleNameBlur}
+              value={warseForm.name}
+              onChange={handleChangeValue}
+              name="name"
+            />
+          </p>
+          <p>
+            助记码:
+            <Input name="code" value={warseForm.code} disabled />
+          </p>
+          <p>
+            所属部门:
+            <Input name="warseId" />
+          </p>
+          <p>
+            联系电话:
+            <Input name="Iphone" />
+          </p>
         </Modal>
       </div>
     </>
